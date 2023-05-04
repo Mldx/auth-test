ed0df6f884ebe5588608655cda66ddfdb784fee1
mldx <anton.zaprutski@gmail.com>
2023-05-05 00:39:29 +0300

    feat: set rtk

---
+++ src/store/slices/userSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+const initialState = {
+  email: null,
+  token: null,
+  id: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser(state, action: PayloadAction<typeof initialState>) {
+      state.email = action.payload.email;
+      state.token = action.payload.token;
+      state.id = action.payload.id;
+    },
+    removeUser(state) {
+      state.email = null;
+      state.token = null;
+      state.id = null;
+    },
+  },
+});
+
+export const userActions = userSlice.actions;
+export const userReducer = userSlice.reducer;
